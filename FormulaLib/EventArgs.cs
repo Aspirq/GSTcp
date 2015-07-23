3821c52eeb983598f21262d206293652e250cc98
IPAsp <a.ponomarenko@ipasp.ru>
2015-07-23 11:29:50 +0500

    Add

---
+++ FormulaLib/EventArgs.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaLib
+{
+    public class EventArgs<T> : EventArgs
+    {
+        private T item;
+
+        public EventArgs(T item)
+        {
+            this.item = item;
+        }
+
+        public T Item
+        {
+            get { return item; }
+        }
+    }
+}
---
+++ FormulaLib/Extension.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FormulaLib
+{
+    public static class Extension
+    {
+        public static string[] Explode(this string source, string separator)
+        {
+            return source.Split(new string[] { separator }, StringSplitOptions.None);
+        }
+
+        public static Boolean IsAnyOf(this Char source, Char[] chars)
+        {
+            for (int i = 0; i < chars.Count(); i++)
+            {
+                if (source == chars[i]) { return true; }
+            }
+            return false;
+        }
+    }
+}
