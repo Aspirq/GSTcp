85c7a620e5d9a8dfcd39751a9c0ced4209cc1e95
IPAsp <a.ponomarenko@ipasp.ru>
2015-07-23 16:26:12 +0500

    References fix

--- GSTcpInLib/GSTcpIn.cs
+++ GSTcpInLib/GSTcpIn.cs
@@ -3,11 +3,39 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
 
 namespace GSTcpInLib
 {
     public class GSTcpIn
     {
-        
+        Thread thread;
+        byte[] b = new byte[4];//IP address
+        Int32 port;
+        NetworkStream stream;
+        TcpClient client;
+
+        public void GSConnect( string GS_IPAddress)
+        {
+            IPAddress addr = IPAddress.Parse(GS_IPAddress);
+            client = new TcpClient();
+
+            try
+            {
+                client.SendTimeout = 3000;
+                client.ReceiveTimeout = 3000;
+                client.Connect(addr, port);
+            }
+            finally
+            {
+
+            }
+
+
+        }
+
+
     }
 }
